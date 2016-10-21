90086f5cb8bf40d3404393dd0014f9997e025aac
Akito Ito <akito_ito@r.recruit.co.jp>
2016-10-21 14:28:54 +0900

    AOJ Volume0 0092 AC

--- aoj/volume0/v0_0092.cpp
+++ aoj/volume0/v0_0092.cpp
@@ -1,106 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <algorithm>
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
 #define rep(i, n) for(int i=0;i<n;i++)
 #define FOR(i, j, n) for(int i=j;i<n;i++)
-#define LEVEL 0
-
-using namespace std;
 
 int N;
-char** data;
-int** dp;
+char data[1000][1000];
+int dp[1000][1000];
 
 int search() {
-
-    rep(i, N) {
-        rep(j, N) {
-        }
-    }
-   
-    return 1;
-}
-
-
-int main() {
-    while(1) {
-        cin >> N;
-        if (N == 0) return 0;
-        data = (char**)malloc(sizeof(char*) * N);
-        rep(i, N) data[i] = (char*)malloc(sizeof(char) * N);
-        dp = (int**)malloc(sizeof(int*) * N);
-        rep(i, N) dp[i] = (int*)malloc(sizeof(int) * N);
-
-        rep(i, N) rep(j, N) cin >> data[i][j];
-        search();
-        cout << dp[N - 1][N - 1] << endl;
-        free(data);
-        free(dp);
-    }
-
-    return 0;
-}
-
-
-/**
-int N = 0;
-char** data;
-int result = 0;
-int search(int x, int y);
-
-int next_search(int x, int y) {
-    int next_x, next_y = 0;
-    if (x == N - 1) {
-        if (y == N - 1) return 0;
-
-        return search(0, y + 1);
-    }
-
-    return search(x + 1, y);
-}
-
-int search(int x, int y) {
-    if (x + result >= N) return next_search(x, y);
-    if (y + result >= N) return 0;
-    if (data[x][y] == '*') return next_search(x, y);
-
-    result = result == 0 ? 1 : result;
-
-   for (int i = 1; i <= N - x; i++) {
-
-        if (y + i > N)
-            return next_search(x, y);
-
-        FOR(k, x, x + i) {
-            FOR(l, y,  y + i) {
-                if(data[k][l] == '.') {
-                    continue;
-                }
-                return next_search(x, y);
+    int max = 0;
+    FOR(i, 1, N) {
+        FOR(j, 1, N) {
+            if(data[i][j] == '*') {
+                dp[i][j] = 0;
+                continue;
             }
+            dp[i][j] = std::min({dp[i][j - 1], dp[i - 1][j - 1], dp[i - 1][j]}) + 1;
+            max = dp[i][j] > max ? dp[i][j] : max;
         }
-        result = result < i ? i : result;
     }
-
-    return next_search(x, y);
+    return max;
 }
-
 
 int main() {
     while(1) {
-        cin >> N;
+        std::cin >> N;
         if (N == 0) return 0;
-        data = (char**)malloc(sizeof(char*) * N);
-        rep(i, N) data[i] = (char*)malloc(sizeof(char) * N);
-        rep(i, N) rep(j, N) cin >> data[i][j];
-        search(0, 0);
-        cout << result << endl;
-        result = 0;
-        free(data);
+        memset(dp, 0, sizeof(dp));
+        rep(i, N) {
+            rep(j, N) std::cin >> data[i][j];
+            dp[i][0] = data[i][0] == '.' ? 1 : 0;
+            dp[0][i] = data[0][i] == '.' ? 1 : 0;
+        }
+
+        std::cout << search() << std::endl;
     }
 
     return 0;
 }
 
-
-**/
