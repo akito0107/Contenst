a8b71e5747db032d81a25a2d25a0f75b135c8b56
Akito Ito <akito0107@gmail.com>
2016-10-30 17:58:45 +0900

    AOJ Volume0 0070 DFS

--- aoj/volume0/v0_0070.cpp
+++ aoj/volume0/v0_0070.cpp
@@ -22,6 +22,7 @@
 #include <string>
 #include <cstring>
 #include <ctime>
+#include <iterator>
 using namespace std;
 
 //repetition
@@ -35,18 +36,39 @@ using namespace std;
 inline int toInt(string s) {int v; istringstream sin(s);sin>>v;return v;}
 template<class T> inline string toString(T x) {ostringstream sout;sout<<x;return sout.str();}
 
+void print_arr(int arr[10]) {
+    cout << "----------------------------" << endl;
+    REP(i, 10) cout << arr[i] << ", ";
+    cout << endl;
+}
+
 //debug
 //-------------------------------------------
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
 int N = 0;
 int S = 0;
-int K = 0;
 int result = 0;
-int dfs(int k, int sum, int n) {
-    if (sum == S) {
+int dfs(int used[10], int sum, int n) {
+    if (sum < 0) return 0;
+    if (n == N + 1) {
+        if (sum == 0) {
+            return 1;
+        }
+        return 0;
     }
-    return result;
+    int res = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (!used[i]) {
+            int buf[10];
+            memcpy(buf, used, 10 * sizeof(int));
+            buf[i] = 1;
+            res += dfs(buf, sum - n * i, n + 1);
+        }
+    }
+
+    return res;
 }
 
 int main() {
@@ -54,13 +76,13 @@ int main() {
     ios::sync_with_stdio(false);
 
     int n, s;
+    int used[10] = {0};
     while(1) {
         cin >> n >> s;
         N = n;
         S = s;
         if (cin.eof()) break;
-        dfs(0, 0, s);
-        cout << result << endl;
+        cout << dfs(used, S, 1) << endl;
     }
     return 0;
 }
