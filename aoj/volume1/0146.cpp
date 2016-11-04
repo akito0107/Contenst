a6abe4ece5bdd659ec02333266350e3242c733cd
Akito Ito <akito0107@gmail.com>
2016-11-04 13:48:58 +0900

    AOJ Volume1 0146 AC

--- aoj/volume1/0146.cpp
+++ aoj/volume1/0146.cpp
@@ -53,24 +53,21 @@ int v[16];
 Pair dp[16][1<<15];
 int N;
 
-Pair solve(int idx, int used, int w) {
+Pair solve(int cnt, int idx, int used, int w) {
     Pair result(0, vector<int>());
     result.second.push_back(s[idx]);
-
-    if (used == 0) return result;
+    if (cnt == N) return result;
     if (dp[idx][used].first != INF) return dp[idx][used];
-
     result.first = INF;
     FOR(i, 1, N + 1) {
-        if((used>>i)&1) {
-            Pair tmp = solve(i, used&~(1<<(i - 1)), w + v[i] * 20);
+        if((used>>(i - 1))&1) {
+            Pair tmp = solve(cnt + 1, i, used&~(1<<(i - 1)), w + v[i] * 20);
             tmp.first += abs(d[i] - d[idx]) / (2000.0 / (70 + w));
             if (result.first > tmp.first) {
                 result = tmp;
             }
         }
     }
-
     result.second.insert(result.second.begin(), s[idx]);
     return dp[idx][used] = result;
 }
@@ -83,13 +80,13 @@ int main() {
     int init = (1<<15) - 1;
     FOR(i, 1, N + 1) cin >> s[i] >> d[i] >> v[i];
     FOR(i, 1, N + 1) {
-        FOR(j, 1, N + 1) {
+        FOR(j, 1, 1<<15) {
             dp[i][j] = Pair(INF, vector<int>());
         }
     }
     Pair ans(INF, vector<int>());
     FOR(i, 1, N + 1) {
-        Pair tmp = solve(i, init&~(1<<i), v[i] * 20);
+        Pair tmp = solve(1, i, init&~(1<<(i - 1)), v[i] * 20);
         if (ans.first > tmp.first) ans = tmp;
     }
 
